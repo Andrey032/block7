b221730c0e0ffebcb47410d13c47907e3b5ed1c8
Andrey Kabishev <kaadron@yandex.ru>
2024-04-05 22:33:10 +0300

    исправление ошибок после ревью

--- src/js/index.js
+++ src/js/index.js
@@ -1,106 +1,101 @@
 import '../scss/style.scss';
+import '../js/swiper/swiper';
 
 const wrapper = document.querySelector('.wrapper');
 const brandList = wrapper.querySelector('.brand__list');
 const servicesList = wrapper.querySelector('.services__list');
 const overlay = wrapper.querySelector('.overlay');
-const modalFeedback = wrapper.querySelector('#feedback');
-const modalCall = wrapper.querySelector('#call');
+const modalFeedback = wrapper.querySelector('.modal-feedback');
+const modalCall = wrapper.querySelector('.modal-call');
+const buttonBrands = wrapper.querySelector('.button-brand');
+const buttonServices = wrapper.querySelector('.button-services');
+const burgerMenu = wrapper.querySelector('.burger');
 
-function toggleBtnBrand(btn) {
-  if (btn.name === 'brand') {
-    brandList.classList.toggle('show-brand');
-    if (brandList.classList.contains('show-brand')) {
-      btn.textContent = 'Скрыть';
-      btn.classList.add('link-btn--revert');
-    } else {
-      btn.textContent = 'Показать все';
-      btn.classList.remove('link-btn--revert');
-    }
+//функция скрыть-показать элементы по кнопке(брэнды)
+function showBrands() {
+  brandList.classList.toggle('show-brand');
+  if (brandList.classList.contains('show-brand')) {
+    buttonBrands.textContent = 'Скрыть';
+    buttonBrands.classList.add('link-btn--revert');
   } else {
-    servicesList.classList.toggle('show-services');
-    if (servicesList.classList.contains('show-services')) {
-      btn.textContent = 'Скрыть';
-      btn.classList.add('link-btn--revert');
-    } else {
-      btn.textContent = 'Показать все';
-      btn.classList.remove('link-btn--revert');
-    }
+    buttonBrands.textContent = 'Показать все';
+    buttonBrands.classList.remove('link-btn--revert');
   }
 }
 
+//функция скрыть-показать элементы по кнопке(услуги)
+function showServices() {
+  servicesList.classList.toggle('show-services');
+  if (servicesList.classList.contains('show-services')) {
+    buttonServices.textContent = 'Скрыть';
+    buttonServices.classList.add('link-btn--revert');
+  } else {
+    buttonServices.textContent = 'Показать все';
+    buttonServices.classList.remove('link-btn--revert');
+  }
+}
+
+//функция скрыть-показать бургер меню
 function toggleBtnMenu() {
   overlay.classList.toggle('show');
 }
 
+//функция показать окно обратный звонок
 function openWindowCall(obj) {
   if (obj.classList.contains('call')) {
     modalCall.classList.add('show');
   }
+}
+
+//функция показать окно обратная связь
+function openWindowChat(obj) {
   if (obj.classList.contains('chat')) {
     modalFeedback.classList.add('show');
   }
 }
 
+//функция закрыть попап
 function closeWindowCall() {
   modalCall.classList.remove('show');
   modalFeedback.classList.remove('show');
   overlay.classList.remove('show');
 }
 
-document.addEventListener('click', function (evt) {
-  if (evt.target.classList.contains('show') && !evt.target.closest('.modal__container')) {
-    closeWindowCall();
-  }
+//функция закрыть бургер меню по области вне меню
+function handleCheckSidebar(evt) {
   if (
     evt.target.classList.contains('show') &&
     !evt.target.closest('.sidebar')
   ) {
     closeWindowCall();
   }
-});
+}
+
+//Слушатели
+buttonBrands.addEventListener('click', showBrands);
+buttonServices.addEventListener('click', showServices);
+burgerMenu.addEventListener('click', toggleBtnMenu);
+overlay.addEventListener('click', handleCheckSidebar);
+
 
 wrapper.addEventListener('click', function (evt) {
-  if (evt.target.closest('.link-btn')) {
-    toggleBtnBrand(evt.target);
+  if (
+    evt.target.classList.contains('show') &&
+    !evt.target.closest('.modal__container')
+  ) {
+    closeWindowCall();
   }
-  if (evt.target.closest('.header-button')) {
-    toggleBtnMenu();
-  }
+});
+
+
+wrapper.addEventListener('click', function (evt) {
   if (evt.target.closest('.call')) {
     openWindowCall(evt.target);
   }
   if (evt.target.closest('.chat')) {
-    openWindowCall(evt.target);
+    openWindowChat(evt.target);
   }
   if (evt.target.closest('.close')) {
     closeWindowCall();
   }
 });
-
-var init = false;
-var swiper;
-function swiperCard() {
-  if (window.innerWidth <= 767) {
-    if (!init) {
-      init = true;
-      swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        spaceBetween: 32,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        }
-      });
-    }
-  } else if (init) {
-    swiper.destroy();
-    init = false;
-  }
-}
-swiperCard();
-window.addEventListener('resize', swiperCard);
-
-console.log('Works!');
